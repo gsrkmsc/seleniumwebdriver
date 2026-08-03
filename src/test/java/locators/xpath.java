6a1e4a687929f522f5e6d339c03bc1aa072665cd
sivaram gundlapudi <gsrkmsc@gmail.com>
2026-08-03 14:45:01 +0530

    Add Thread.sleep to allow footer to render before locating element

--- src/test/java/locators/xpath.java
+++ src/test/java/locators/xpath.java
@@ -7,16 +7,18 @@ import java.util.List;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import java.time.Duration;
 
 public class xpath {
 	
 	@Test(priority=1)
 	
-	public  void xpath() {
+	public  void xpath()  {
 		
 		
 	
 	WebDriver driver=new ChromeDriver();
+	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 	driver.manage().window().maximize();
 	driver.navigate().to("https://www.amazon.in/");
 	//aboslute xpath
@@ -26,6 +28,7 @@ public class xpath {
 	//xpath with text only not using any attribute
 	//driver.findElement(By.xpath("a[text()='Bestsellers']")).click();
 	//text
+	
 	driver.findElement(By.xpath("//span[contains(text(),'Amazon.com, Inc. or its affiliates')]")).isDisplayed();
 	driver.close();
 
